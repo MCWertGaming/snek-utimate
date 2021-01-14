006b8d2a95ec3e4d65ec5c01eb79c61ac0a0eee4
damon <mcwertgamingQgmail.com>
2021-01-14 14:12:26 +0000

    improved unit testing

---
+++ .ci/compile.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -ex
+
+CLEANUP()
+{
+    git reset --hard
+    git clean -fdx
+}
+RUN_PROGRAMMS()
+{
+    # a list of all programs / targets compiled by this project
+    ./tests/testing-target
+}
+
+# compile gcc release
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+CLEANUP
+cmake -DCMAKE_INSTALL_PREFIX=`pwd`/inst .
+cmake --build . --config Release
+RUN_PROGRAMMS
+
+# compile gcc debug
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+CLEANUP
+cmake -DCMAKE_INSTALL_PREFIX=`pwd`/inst .
+cmake --build . --config Debug
+RUN_PROGRAMMS
+
+# compile clang release
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+CLEANUP
+cmake -DCMAKE_INSTALL_PREFIX=`pwd`/inst .
+cmake --build . --config Release
+RUN_PROGRAMMS
+
+# compile clang debug
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+CLEANUP
+cmake -DCMAKE_INSTALL_PREFIX=`pwd`/inst .
+cmake --build . --config Debug
+RUN_PROGRAMMS
--- ci/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/inst .
-cmake --build . --config Release
-cmake --build . --config Debug
